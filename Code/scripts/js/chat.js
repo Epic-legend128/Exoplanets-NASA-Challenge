d9b765e2504542c5d1df748c4d81ef0bf495ebd3
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-10-05 14:09:07 +0200

    Adding the chat strucutre

---
+++ Code/scripts/js/chat.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("body")[0].innerHTML += "<form id='chat-container'><div id='chat-prev'></div><input id='chat-input' type='text' placeholder='Enter prompt...'></form>";
+document.getElementsByTagName("body")[0].innerHTML += "<button value='chat-button'>Chat</button>";
+
+console.log("Added everything");
